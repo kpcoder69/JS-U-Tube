cfd4ba7eae1a51bda2f73f978649773de74945af
Karan pandey <110976645+kpcoder69@users.noreply.github.com>
2025-01-01 17:40:39 +0000

    Lecture 5

---
+++ 01_basics/02_DataType.js
@@ -0,0 +1,37 @@
+"Use Strict"; //Treat all JS code as Newer Version
+
+//alert(3+3)  its give error bcz we are not using browser we are using node js
+
+console.log("Karan"); console.log(3+3) //it is right code an execute also even though its not a good programming practice
+
+console.log(3+
+    3)  //its also execuated but readiblity of the code is very bad 
+
+console.log("KP")
+
+
+
+let name = "hitesh"  //it is in string type
+let age = 18
+let isLoggedIn  = false
+let state = null
+
+
+//there are different different Datatype in javaScript some of them are given below
+
+// number => 2 to power 53
+// bigInt => it is consider to take big number but minimum use 
+// String => " "(we say group characters)
+// boolean => standalone value
+// undefined => i.e undefine
+//symbol => unique        ======> these all about premitive data type
+
+
+//object is also data type but it is not primitive
+
+console.log(typeof name);
+console.log(typeof null); // output give null is object type very imporatnt question on interview point of view
+console.log(typeof undefined); // output is also give undefined
+
+
+
