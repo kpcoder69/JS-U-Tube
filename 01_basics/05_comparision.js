87fd1b57e276bf2ceeeca9d29d97c13173dd7227
Karan pandey <110976645+kpcoder69@users.noreply.github.com>
2025-01-02 16:52:33 +0000

    Lec 8

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,29 @@
+/*console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==6);
+console.log(2 != 1);*/
+
+
+//whenever we compare any value always rememmber that boht are same data type
+/*console.log("2" > 1);
+console.log("02" > 1);*/
+
+// comparision on null
+/*console.log(null>0);
+console.log(null == 0);
+console.log(null >= 0);
+                                     // ====> aise wale comparisoion confusion create karte hai bahut
+//comparision on undefined
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);*/
+
+//strict check....(which represent by === its check the data type of the comparision value)
+console.log("2" === 2); // now its check its dataType also
+
+
+
+
+
+
