3bed344899ac9ea0afce637cde36a269c3ac42a8
Karan pandey <110976645+kpcoder69@users.noreply.github.com>
2025-01-02 11:11:34 +0000

    Lec 7

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,65 @@
+// ******************OPERATION********************
+
+// let value = 3
+// let NegValue = -value
+
+// console.log(NegValue);
+
+
+// console.log(2+2); //addition 
+// console.log(2-2); //substraction
+// console.log(2*3); //multipication
+// console.log(2**3); //power of 2 is 3
+// console.log(5/3); //division
+// console.log(7%5); // modules use for reminder
+
+
+//now see some confusion in String
+
+/*let str1 = "Hello"
+let str2 = " Karan"
+let str3 = str1+str2
+console.log(str3);  // yet very simple its also called concetination in java
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+"2"+"3");
+console.log("1"+2+2);
+
+//now this is totally diiferent bcz of ToPrimitive is concept in very advance we no need to know yet
+console.log(1+2+"2");*/
+
+// these are some concept which have no use in production but exam point of view it also valid...may be asked
+/*console.log((3+4)*5%3);
+console.log(+true);
+console.log(+"");
+let num1, num2, num3
+num1=num2=num3= 3+4
+console.log(num1);
+console.log(num2);
+console.log(num3);*/
+
+// Note :- code readablity is most priority
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
