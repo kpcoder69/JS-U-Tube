a984ee4521288fb11bdff1232c0d05e290f70120
Karan pandey <110976645+kpcoder69@users.noreply.github.com>
2025-01-02 10:40:44 +0000

    Lec 6

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,10 +1,45 @@
-let score = 33
-// let score = "hitesh"
-// let score = "undefine"
+/*// let score = 33
+// let score = "33abc"
+// let score = "hitesh"         //un comment line wise line and try all the inputs
+// let score = undefined
+// let score = null     
+// let score = true
+// let score = false
+
+console.log(score);
 
 
 console.log(typeof score);
-console.log(typeof typeof(score));   //both are give same output
+console.log(typeof (score));   //both are give same output
+
+// so conclusion is that these all are not show actual datatype its create problem when we reach next level even though score variable is execute
+
+//Write way is....(or we say its convert other Datatype to Number)
+let valueInNumber = Number(score) // here valueInNumber convert all the datatype in number there for its type always show number
+
+console.log(typeof valueInNumber); // here we got confusion the value of score "33" then how its give..number so 
+                                    //these are some concepts in javaScript is not define in well manner
+
+console.log(valueInNumber);   // NaN means Not A number    */
+
+//now we convert other datatype to Boolean
+// let Value = 1
+// let Value = 0
+// let Value = 345
+// let Value = "Karan"
+/*let Value = " "
+let BooleanValue =  Boolean(Value)
+console.log(BooleanValue)*/
+
+
+//Now convert into String
+let value = "66"
+
+let StringValue = String(value)
+console.log(StringValue);
+
+console.log(typeof StringValue);
+
 
 
 
