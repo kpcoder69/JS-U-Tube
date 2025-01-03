710e7fd82515000b1a54aec80d58eff5e6bd35c6
Karan pandey <110976645+kpcoder69@users.noreply.github.com>
2025-01-03 13:46:45 +0000

    Lec 11

---
+++ 01_basics/08_String.js
@@ -0,0 +1,14 @@
+const name = "Karan"
+const repoCount = 50
+
+// console.log(name+repoCount+" value"); this is the noraml way to print String and string concetination
+
+// in devlopment line this one is best way we also called String interpullation (` ${} ${} `)
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+//another way of string is...........
+const gameName  = new String('karan Pandey')
+
+console.log(gameName[5]);
+
+
